9afc8c312930c6f29b0c172507f4509f74996336
Harmoniq Punk <spiny_05giggly@icloud.com>
2024-05-28 23:06:32 +0200

    Setup WIP

---
+++ src/OS4/OS4App.swift
@@ -0,0 +1,17 @@
+//
+//  OS4App.swift
+//  OS4
+//
+//  Created by George on 23.05.24.
+//
+
+import SwiftUI
+
+@main
+struct OS4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
